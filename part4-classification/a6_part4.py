ef800a34b4becd61483fb6f7ad9de0a014617e52
Bruno Pena-Lepe <bpena-lepe@cps.edu>
2023-12-13 11:46:19 -0600

    working_on_4

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -19,12 +19,34 @@ x = scaler.transform(x)
 # Step 3: Transform the data
 
 # Step 4: Split the data into training and testing data
-
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 # Step 5: Fit the data
-
+model = linear_model.LogisticRegression().fit(x_train, y_train)
 # Step 6: Create a LogsiticRegression object and fit the data
 
 # Step 7: Print the score to see the accuracy of the model
+print("Accuracy:", model.score(x_test, y_test))
+print("*************")
 
 # Step 8: Print out the actual ytest values and predicted y values
 # based on the xtest data
+print("Testing Results:")
+print("")
+#print(y_test)
+for index in range(len(x_test)):
+    x = x_test[index]
+    x = x.reshape(-1, 3)
+    y_pred = int(model.predict(x))
+
+    if y_pred == 0:
+        y_pred = "not purchased"
+    else:
+        y_pred = "purchased"
+    
+    actual = y_test[index]
+    if actual == 0:
+        actual = "not purchased"
+    else:
+        actual = "purchased"
+    print("Purchase prediction: " + y_pred + "Real : " + actual)
+    print("")
